e863e3a869acdba2ecfc96d05cc4b68886a4cc4c
Brad Murphy <bsmptc@gmail.com>
2012-08-06 11:50:18 -0400

    support for canceling file uploads now working part II

---
+++ src/java/src/plupload/UploadCanceledException.java
@@ -0,0 +1,21 @@
+package plupload;
+
+public class UploadCanceledException extends Exception {
+
+	public UploadCanceledException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UploadCanceledException(String arg0) {
+		super(arg0);
+	}
+
+	public UploadCanceledException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public UploadCanceledException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
